ab5ddaeeac48d52c7484fd50ba75ad91a0edabbc
Meghamsh <megha@bio172037.college.tcd.ie>
2025-11-30 10:34:29 +0000

    Use relative WSL path in launcher

--- Run-App.bat
+++ Run-App.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
-set "WSL_PATH='<PROJECTS_DIR>/Y-maze-randomizer/modern-app'"
-start "Y-maze servers" wsl -e bash -lc "cd %WSL_PATH% && npm run dev:full"
+for /f "usebackq tokens=*" %%i in (`wsl wslpath "%~dp0"`) do set WSL_DIR=%%i
+start "Y-maze servers" wsl -e bash -lc "cd \"%WSL_DIR%modern-app\" && npm run dev:full"
 timeout /t 4 >nul
 start "" http://localhost:5175
 endlocal
